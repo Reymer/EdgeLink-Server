8780db1764fac4f09a8f89a20a01492e81cab5cb
Extra <changlin.cai@yunhetech.com.tw>
2024-10-25 19:20:21 +0800

    [修改]uiKey

--- Assets/DevKit/UICollector/UIKey.cs
+++ Assets/DevKit/UICollector/UIKey.cs
@@ -17,8 +17,6 @@ namespace DevKit.Tool
         
         public const string UI_clear = "UI_clear";
         
-        public const string UI_Mask = "UI_Mask";
-        
         public const string UI_Tables = "UI_Tables";
         
         public const string UI_MenuRoot = "UI_MenuRoot";
@@ -33,10 +31,6 @@ namespace DevKit.Tool
         
         public const string UI_LocalPortsMask = "UI_LocalPortsMask";
         
-        public const string UI_DropdownRoot = "UI_DropdownRoot";
-        
-        public const string UI_MaskRoot = "UI_MaskRoot";
-        
         public const string UI_NetProtrocolTitle = "UI_NetProtrocolTitle";
         
         public const string UI_RemotePortInput = "UI_RemotePortInput";
@@ -49,25 +43,9 @@ namespace DevKit.Tool
         
         public const string UI_NetProtocolDropdowm = "UI_NetProtocolDropdowm";
         
-        public const string UI_MaskDropdown = "UI_MaskDropdown";
-        
         public const string UI_NameInput = "UI_NameInput";
         
-        public const string UI_MaskToggle = "UI_MaskToggle";
-        
-        public const string UI_MaskOK = "UI_MaskOK";
-        
-        public const string UI_MaskDeleteButton = "UI_MaskDeleteButton";
-        
-        public const string UI_MaskCancel = "UI_MaskCancel";
-        
-        public const string UI_PacketDataNameInput = "UI_PacketDataNameInput";
-        
-        public const string UI_PacketStartMarkerInput = "UI_PacketStartMarkerInput";
-        
-        public const string UI_PacketEndMarkerInput = "UI_PacketEndMarkerInput";
-        
-        public const string UI_MaxPacketSizeInput = "UI_MaxPacketSizeInput";
+        public const string UI_DropdownMask = "UI_DropdownMask";
         
         public const string table_prococolText = "table_prococolText";
         
@@ -82,6 +60,8 @@ namespace DevKit.Tool
         public const string table_ForwardTargetText = "table_ForwardTargetText";
         
         public const string table_netReceivedStatus = "table_netReceivedStatus";
+        
+        public const string table_DropdownMask = "table_DropdownMask";
         
         public const string table_Delete = "table_Delete";
         
